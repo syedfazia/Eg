9e74eb8432b62d5953843affdcb59441d842ad27
Syed Fazia <118902039+syedfazia@users.noreply.github.com>
2023-07-04 10:29:07 +0530

    Update first.c

--- first.c
+++ first.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int sum(int a,int b);
+
 int main(int argc, char *argv[]) {
 	int a=4,b=5;
-	int ans=sum(a,b);
+	int ans=a+b;
 	printf("%d",ans);
 	return 0;
 }
